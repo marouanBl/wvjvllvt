9ff7eb0c894f1cf0077343c65f2f6959a44dc50c
Hatchvenom Slim <naughtydog_slim@yahoo.fr>
2013-01-26 16:26:12 +0100

    Some edit section route fixing

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Unbound5::Application.routes.draw do
 
   root :to => 'journals#index'
 
-resources :users
-resources :sessions
+
 
 get "logout" => "sessions#destroy", :as => "logout"
 get "login" => "sessions#new", :as => "login"
 get "signup" => "users#new", :as => "signup"
 
 
-  resources :categories
+  resources :users
+resources :sessions
+resources :categories
 
 
   resources :templates
