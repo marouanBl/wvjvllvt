ce4a1d6d121fffe1489e1f296a8a208bbad1494d
Hatchvenom Slim <naughtydog_slim@yahoo.fr>
2013-01-26 19:57:11 +0100

    Pages to templates: ok

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,7 @@ class Issue < ActiveRecord::Base
 
 	#un articles peut paraitre dans plusieurs numeros, et un numero a pl
 	belongs_to :journal
+	has_many :pages
 
   attr_accessible :has_been_released, :issue_intro, :journal_id, :number
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,7 @@
 class Page < ActiveRecord::Base
 
 	belongs_to :template
+	belongs_to :issue
 
   attr_accessible :issue_id, :number, :template_id
 end
