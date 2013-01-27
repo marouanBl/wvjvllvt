2647a3960563c6f997b9daa7dd883452275c3c58
Hatchvenom Slim <naughtydog_slim@yahoo.fr>
2013-01-27 10:21:38 +0100

    working on home, jquery still shitty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 	def create
 	  @user = User.new(params[:user])
 	  if @user.save
-	    redirect_to edit_user_path(params[:user][:id]), :notice => "Signed up! Please Take a moment to edit your data before continuing on"
+	    redirect_to journals_path, :notice => "Signed up! Please Take a moment to edit your data before continuing on"
 	  else
 	    render :new
 	  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :articles
   has_many :templates
   
-  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation, :first_name, :last_name
 
   validates_uniqueness_of :email
 end
